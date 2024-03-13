08206de2cdde21bf3b3f02cb3a1d5008b788cfac
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-03-13 17:25:41 +0500

    register Applications services

--- MarketPlace.API/Program.cs
+++ MarketPlace.API/Program.cs
@@ -1,4 +1,7 @@
 
+using MarketPlace.Application;
+using MarketPlace.Infrastructure;
+
 namespace MarketPlace.API
 {
     public class Program
@@ -8,6 +11,8 @@ namespace MarketPlace.API
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddInfrastruct(builder.Configuration);
+            builder.Services.AddAppLication();
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
