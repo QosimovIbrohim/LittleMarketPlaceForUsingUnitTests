c83d95bb21a6cde2c4fb17c50d70064510164c63
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-03-13 16:44:50 +0500

    add product

--- MarketPlace.Domain/Entities/Product.cs
+++ MarketPlace.Domain/Entities/Product.cs
@@ -8,5 +8,8 @@ namespace MarketPlace.Domain.Entities
 {
     public class Product
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
     }
 }
