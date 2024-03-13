2130e25274b6115467fdefa21f03440b62ad43aa
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-03-13 17:26:26 +0500

    `IProductService` and `ProductService` Added and Scoped

---
+++ MarketPlace.Application/Abstractions/IProductService.cs
@@ -0,0 +1,19 @@
+﻿using MarketPlace.Domain.DTOs;
+using MarketPlace.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketPlace.Application.Abstractions
+{
+    public interface IProductService
+    {
+        public Task<List<Product>> GetAll();
+        public Task<Product> Get(int id);
+        public Task<string> Add(ProductDTO pr);
+        public Task<string> Delete(int id);
+        public Task<string> Update(ProductDTO product);
+    }
+}
---
+++ MarketPlace.Application/Services/ProductService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MarketPlace.Application.Abstractions;
+using MarketPlace.Domain.DTOs;
+using MarketPlace.Domain.Entities;
+
+namespace MarketPlace.Application.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductService(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<string> Add(ProductDTO pr)
+        {
+            var product = MapDTOToEntity(pr);
+            await _productRepository.InsertAsync(product);
+            return "Product added successfully";
+        }
+
+        public async Task<string> Delete(int id)
+        {
+            var result = await _productRepository.DeleteAsync(id);
+            return result;
+        }
+
+        public async Task<Product> Get(int id)
+        {
+            var product = await _productRepository.GetByIdAsync(id);
+            return product;
+        }
+
+        public async Task<List<Product>> GetAll()
+        {
+            var products = await _productRepository.GetAllAsync();
+            return products;
+        }
+
+        public async Task<string> Update(ProductDTO product)
+        {
+            var entity = MapDTOToEntity(product);
+            await _productRepository.UpdateAsync(entity);
+            return "Product updated successfully";
+        }
+
+        private Product MapDTOToEntity(ProductDTO dto)
+        {
+            return new Product
+            {
+                Name = dto.Name,
+                Description = dto.Description
+            };
+        }
+    }
+}
