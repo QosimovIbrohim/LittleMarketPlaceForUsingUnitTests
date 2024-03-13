ecde14163f42ab25bafb0a856af1a017128989f9
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-03-13 17:05:26 +0500

    Taskga o'rab qoytardim

---
+++ MarketPlace.Application/Abstractions/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using MarketPlace.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketPlace.Application.Abstractions
+{
+    public interface IProductRepository
+    {
+        public Task<List<Product>> GetAllAsync();
+        public Task<Product> GetByIdAsync(int id);
+        public Task<string> InsertAsync(Product pr);
+        public Task<string> DeleteAsync(int id);
+        public Task<string> UpdateAsync(Product product);
+    }
+}
